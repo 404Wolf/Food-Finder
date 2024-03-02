2b60f790a1399d0fe44dbccba64052569ef9ff4f
Wolf <caffeinate@msn.com>
2024-03-01 21:50:19 -0500

    Add docker push script

--- Event-Collector/googlePush.sh
+++ Event-Collector/googlePush.sh
@@ -1,2 +1,3 @@
+docker build -t event-collector .
 docker tag event-refresher us-east1-docker.pkg.dev/secure-brook-415417/refresh-events/event-refresher:latest
 docker push us-east1-docker.pkg.dev/secure-brook-415417/refresh-events/event-refresher:latest
