ba1fa624dc7bccd51b1bd88284bc52d7fa86f394
Wolf <caffeinate@msn.com>
2024-03-01 21:42:27 -0500

    Add dockerator sh script

---
+++ Event-Collector/googlePush.sh
@@ -0,0 +1,2 @@
+docker tag event-refresher us-east1-docker.pkg.dev/secure-brook-415417/refresh-events/event-refresher:latest
+docker push us-east1-docker.pkg.dev/secure-brook-415417/refresh-events/event-refresher:latest
