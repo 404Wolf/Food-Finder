7bb735b7695a3cf885ee1bd5bb98f49e95c0a86c
Wolf <caffeinate@msn.com>
2024-03-05 11:44:02 -0500

    Add metadata

--- food-finder/src/app/page.tsx
+++ food-finder/src/app/page.tsx
@@ -1,23 +1,17 @@
 import { EventsArea } from "@/components/events/EventsArea";
 import Appbar from "@/components/Appbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Food Finder",
+    applicationName: "Food Finder",
+    description: "Find Case Western University events that offer food",
+    keywords: "Case Western, food, events, free, CWRU, university",
+    abstract:
+        "Tool to locate free food at Case Western Reserve University by analyzing all events on campus and filtering them by whether they offer food and of what type.",
+};
 
 export default async function Home() {
-    /*
-    async function getEvents() {
-        "use server";
-        const res = await fetch("http://localhost:3000/api/events");
-        const data = await res.json();
-        return data.data;
-    }
-
-    async function getCuisines() {
-        "use server";
-        const res = await fetch("http://localhost:3000/api/events/cuisines");
-        const data = await res.json();
-        return data.data;
-    }
-    */
-
     return (
         <>
             <Appbar />
